89a0d419d0dd5a279847597c5bbfb6765ec67fda
Marcelo Flores <marfloralv@gmail.com>
2025-08-26 18:46:44 -0600

    running commands on the terminal with subprocess library

--- Exercise Files/practice.py
+++ Exercise Files/practice.py
@@ -1,4 +1,4 @@
-import subprocess
+import subprocess # library to use the command line
 
 for i in range(5):
-    subprocess.check_call(["python3", "example.py"])
+    subprocess.check_call(["python3", "example.py"]) # Executing the terminal command for running example.py 5 times on the termminal
