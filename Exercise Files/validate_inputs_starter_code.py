580bfd59891c7af3fb0ed72af364aa8060896549
Marcelo Flores <marfloralv@gmail.com>
2025-08-27 22:19:40 -0600

    input validation

--- Exercise Files/validate_inputs_starter_code.py
+++ Exercise Files/validate_inputs_starter_code.py
@@ -3,23 +3,23 @@ import pyinputplus as pyip
 print("\nEXAMPLE 1")
 
 # uncomment the following line of code and fill in 
-# result = 
+result = pyip.inputInt("Enter the number of shopping bags you will need for your items:", min=0) # This asks the user to enter the number of bags an checks that it is an integer and it's equal or greater than 0 
 
 # uncomment the following line of code
-# print("\nYou will use", result, "store bags.")
+print("\nYou will use", result, "store bags.")
 
 print("\nEXAMPLE 2")
 
 # uncomment the following line of code and fill in 
-# result = 
+result = pyip.inputMenu(["red", "blue", "green"], lettered=True, numbered=False)
 
 # uncomment the following line of code
-# print("\nYou have chosen a", result, "marker.")
+print("\nYou have chosen a", result, "marker.")
 
 print("\nEXAMPLE 3")
 
 # uncomment the following line of code and fill in 
-# result = 
+result = pyip.inputEmail("Enter your email address:")
 
 # uncomment the following line of code
-# print("\nThe email you entered:", result)
+print("\nThe email you entered:", result)   
